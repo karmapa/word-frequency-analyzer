5c444532b476619987d1516926f7ff9ce5877241
kmsheng <kmsh3ng@gmail.com>
2017-06-14 23:12:56 +0800

    use Array.prototype.map to generate result rows

--- src/components/AnalyzedResult/AnalyzedResult.js
+++ src/components/AnalyzedResult/AnalyzedResult.js
@@ -2,15 +2,9 @@ import React from 'react'
 
 const AnalyzedResult = ({words = [], times = []}) => {
 
-  let finalResult = [];
+  const resultRows = words.map((word, index) => (<div key={`result-row-${index}`}>{word}<hr />{times[index]}</div>));
 
-  const renderMoods = () => {
-    for (let i in words) {
-      finalResult.push(<div>{words[i]}<hr/>{times[i]}</div>)
-    }
-    return finalResult
-  }
-  return <div id="showResult">{renderMoods()}</div>;
+  return <div id="showResult">{resultRows}</div>;
 };
 
 export default AnalyzedResult;
