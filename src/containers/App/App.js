2df94853777773583c1f059a25758fe4fc959fb9
kmsheng <kmsh3ng@gmail.com>
2017-06-14 22:47:49 +0800

    re-style App.js

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import ListPage from './../../components/ListPage';
 
 class App extends Component {
   render() {
     return (
-      <ListPage/>
+      <ListPage />
     );
   }
 }
