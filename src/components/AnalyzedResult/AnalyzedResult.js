73c06ce2dd2a03a1770d860e763ffc683d346d17
kmsheng <kmsh3ng@gmail.com>
2017-06-14 23:04:15 +0800

    re-indent AnalyzedResult.js

--- src/components/AnalyzedResult/AnalyzedResult.js
+++ src/components/AnalyzedResult/AnalyzedResult.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 const AnalyzedResult = (props: {words: [], times: []}) => {
 
-let finalResult = [];
+  let finalResult = [];
 
-const renderMoods = () => {
-for (let i in props.words) {
-finalResult.push(<div>{props.words[i]}<hr/>{props.times[i]}</div>)
-}
-return finalResult
-}
+  const renderMoods = () => {
+    for (let i in props.words) {
+      finalResult.push(<div>{props.words[i]}<hr/>{props.times[i]}</div>)
+    }
+    return finalResult
+  }
   return <div id="showResult">{renderMoods()}</div>;
 };
 
