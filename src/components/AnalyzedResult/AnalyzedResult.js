58c471ae5096803dbc9e3b06ef753ee76a3aeba0
kmsheng <kmsh3ng@gmail.com>
2017-06-14 23:00:54 +0800

    re-style

--- src/components/AnalyzedResult/AnalyzedResult.js
+++ src/components/AnalyzedResult/AnalyzedResult.js
@@ -1,26 +1,16 @@
 import React from 'react'
 
-
 const AnalyzedResult =(props:{words:[],times:[]})=> {
-
 
 let finalResult=[];
 
 const renderMoods=()=> {
-
 for (let i in props.words){
 finalResult.push(<div>{props.words[i]}<hr/>{props.times[i]}</div>)
 }
-
 return finalResult
-
 }
-
-
   return <div id="showResult">{renderMoods()}</div>;
-
-
 };
-
 
 export default AnalyzedResult;
