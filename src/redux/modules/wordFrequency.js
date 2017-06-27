598e1ad1d445f68c6048a95695866a4aa57e5ccc
kmsheng <kmsh3ng@gmail.com>
2017-06-27 22:02:57 +0800

    add function getPb

--- src/redux/modules/wordFrequency.js
+++ src/redux/modules/wordFrequency.js
@@ -1,7 +1,24 @@
+export const GET_PB = 'word-frequency-analyzer/wordFrequency/GET_PB';
+export const GET_PB_SUCCESS = 'word-frequency-analyzer/wordFrequency/GET_PB_SUCCESS';
+export const GET_PB_FAIL = 'word-frequency-analyzer/wordFrequency/GET_PB_FAIL';
+
 const initialState = {
   analyzedData: {}
 };
 
 export default function wordFrequency(state = initialState, action) {
   return state;
+}
+
+export function getPb({kdbName, pbId}) {
+  return {
+    types: [GET_PB, GET_PB_SUCCESS, GET_PB_FAIL],
+    promise: (fetch) => fetch(`https://api.dharma-treasure.org/kdbs/${kdbName}/pbs/${pbId}`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        return Promise.reject(response);
+      })
+  };
 }
