e2e01189539184d5ccb66eb23590432578d874b7
kmsheng <kmsh3ng@gmail.com>
2017-06-27 22:02:06 +0800

    apply client middleware ( using fetch )

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
+import clientMiddleware from './redux/middlewares/clientMiddleware';
 import rootReducer from './redux/modules/rootReducer';
 import App from './containers/App/App';
 import './index.css';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(clientMiddleware(fetch)));
 
 const component = (
   <Provider store={store}>
