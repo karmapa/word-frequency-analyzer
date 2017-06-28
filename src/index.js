7eba5c21a54047684d47a1f2d068023613996fea
Huang Li-Yu <Alice-H@users.noreply.github.com>
2017-06-28 10:17:15 +0800

    use provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import rootReducer from './redux/modules/rootReducer';
 import App from './containers/App/App';
 import './index.css';
 
 const store = createStore(rootReducer)
 
+const component = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
 ReactDOM.render(
-  <App />,
+  component,
   document.getElementById('root')
 );
