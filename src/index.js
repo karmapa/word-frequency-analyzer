e5c4ee3cf9a5ff10ad144a3b9e7ee94823c45c94
Huang Li-Yu <Alice-H@users.noreply.github.com>
2017-06-28 10:03:59 +0800

    create store for single source

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import rootReducer from './redux/modules/rootReducer';
 import App from './containers/App/App';
 import './index.css';
+
+const store = createStore(rootReducer)
 
 ReactDOM.render(
   <App />,
