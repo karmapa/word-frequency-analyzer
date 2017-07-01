820cbd54ab0725b47749523bcf77ae28e45d7d26
Huang Li-Yu <Alice-H@users.noreply.github.com>
2017-07-01 17:52:59 +0800

    import react-router

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {BrowserRouter as Router, Link, Match, Miss} from 'react-router'
 import './App.css';
 import PageWordFrequency from './../PageWordFrequency/PageWordFrequency';
 
