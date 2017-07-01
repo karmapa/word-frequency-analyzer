a12d2937fe34f51c3ac1b5131044360cb3774b26
Huang Li-Yu <Alice-H@users.noreply.github.com>
2017-07-01 21:04:25 +0800

    build RouterPath

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,14 +1,28 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Link, Match, Miss} from 'react-router'
+import {BrowserRouter as Router, Route , Link} from 'react-router-dom'
 import './App.css';
 import PageWordFrequency from './../PageWordFrequency/PageWordFrequency';
+import About from './../About/About'
 
 class App extends Component {
 
   render() {
+
     return (
       <div className="App">
-        <PageWordFrequency />
+        <Router>
+          <div>
+            <ul>
+              <li><Link to='/'>Home</Link></li>
+              <li><Link to='/about'>About</Link></li>
+              <li><Link to='/wordFrequency'>Word Frequency</Link></li>
+            </ul>
+            <hr/>
+            <Route exact path='/' component={PageWordFrequency} />
+            <Route path='/about' component={About} />
+            <Route path='/wordFrequency' component={PageWordFrequency} />
+          </div>
+        </Router>
       </div>
     );
   }
