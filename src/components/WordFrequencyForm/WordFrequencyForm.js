1f948fb392f1307613354ed8bbb0145d14115af1
Huang Li-Yu <Alice-H@users.noreply.github.com>
2017-07-05 11:27:52 +0800

    rename the function

--- src/components/WordFrequencyForm/WordFrequencyForm.js
+++ src/components/WordFrequencyForm/WordFrequencyForm.js
@@ -10,7 +10,7 @@ class WordFrequencyForm extends Component {
     onSubmit: PropTypes.func.isRequired
   };
 
-  kdbNameInput({ input, meta: { touched, error }, ...custom }) {
+  handleInput({ input, meta: { touched, error }, ...custom }) {
     const hasError = touched && error !== undefined;
     return (
       <div>
@@ -38,11 +38,11 @@ class WordFrequencyForm extends Component {
         <label>
           <span>KDB名稱：</span>
         </label>
-        <Field name="kdbName" component={this.kdbNameInput} type="text" className="input" />
+        <Field name="kdbName" component={this.handleInput} type="text" className="input" />
         <label>
           <span>PB ID：</span>
         </label>
-        <Field name="pbId" component={this.kdbNameInput} type="text" className="input" />
+        <Field name="pbId" component={this.handleInput} type="text" className="input" />
         <button type="submit">Search</button>
       </form>
     );
