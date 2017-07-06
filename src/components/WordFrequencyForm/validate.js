9aef13c696ad6239a751c30068231118c5cbbad5
Huang Li-Yu <Alice-H@users.noreply.github.com>
2017-07-06 19:13:27 +0800

    revised the coding style

--- src/components/WordFrequencyForm/validate.js
+++ src/components/WordFrequencyForm/validate.js
@@ -2,13 +2,13 @@ const validate = (values) => {
   const errors = {};
   const pbRegRule = new RegExp (/^\d{1}-\d{1}-\d{1}[a-zA-Z]$/);
 
-  if (!values.kdbName || '' === values.kdbName.trim()) {
+  if (! values.kdbName || '' === values.kdbName.trim()) {
     errors.kdbName = 'KDB 名稱是必填欄位';
   }
-  else if (!values.pbId || '' === values.pbId.trim()) {
+  else if (! values.pbId || '' === values.pbId.trim()) {
     errors.pbId = 'PB ID 是必填欄位';
   }
-  else if (!pbRegRule.test(values.pbId)) {
+  else if (! pbRegRule.test(values.pbId)) {
     errors.pbId = '格式範例：1-1-1a';
   }
   return errors;
