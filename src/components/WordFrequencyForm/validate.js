46a48c576c29f199d89e0ab26885833991c8a93a
Huang Li-Yu <Alice-H@users.noreply.github.com>
2017-07-06 19:12:16 +0800

    revised the coding style

--- src/components/WordFrequencyForm/validate.js
+++ src/components/WordFrequencyForm/validate.js
@@ -1,11 +1,11 @@
-const validate = values => {
+const validate = (values) => {
   const errors = {};
   const pbRegRule = new RegExp (/^\d{1}-\d{1}-\d{1}[a-zA-Z]$/);
 
   if (!values.kdbName || '' === values.kdbName.trim()) {
     errors.kdbName = 'KDB 名稱是必填欄位';
   }
-  else  if (!values.pbId || '' === values.pbId.trim()) {
+  else if (!values.pbId || '' === values.pbId.trim()) {
     errors.pbId = 'PB ID 是必填欄位';
   }
   else if (!pbRegRule.test(values.pbId)) {
